fce0e1e2eaabd7a39ef42ae96d9fed246c630d13
95mishraankur <61880741+95mishraankur@users.noreply.github.com>
2022-05-23 12:06:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+import { Server } from "socket.io";
+import axios from "axios";
+const io = new Server({
+  cors: {
+    origin: "http://127.0.0.1:3333",
+  },
+});
+
+let onlineUsers = [];
+
+const addNewUser = (user, socketId) => {
+  !onlineUsers.some((users) => users.user === user) &&
+    onlineUsers.push({ user, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (id) => {
+
+  return onlineUsers.find((users) => users.user === id);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (user) => {
+    addNewUser(user, socket.id);
+  });
+
+  socket.on("sendNotification", ({ id, action }) => {
+    console.log(id, action)
+    id.map((data) => {
+      const receiver = getUser(data);
+      console.log(receiver, onlineUsers, 'recever')
+      if (receiver != undefined)
+        io.to(receiver.socketId).emit("getNotification", {
+          id,
+          action
+        });
+    })
+
+  });
+
+  // socket.on("sendText", ({ senderName, receiverName, text }) => {
+  //   const receiver = getUser(receiverName);
+  //   io.to(receiver.socketId).emit("getText", {
+  //     senderName,
+  //     text,
+  //   });
+  // });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id, 'disconnect')
+    removeUser(socket.id);
+    console.log(onlineUsers)
+  });
+});
+
+io.listen(6000);
